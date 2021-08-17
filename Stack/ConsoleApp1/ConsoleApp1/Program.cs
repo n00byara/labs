9f00b6420e2a3ca7773f9d93b98c7039b173366b
n00byara <aminovmikl.01@gmail.com>
2021-08-17 17:19:56 +0300

    stack start

---
+++ Stack/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new[] { 1, 2, 3 };
+            Stack stack = new Stack(arr);
+            stack.Push(1);
+            stack.Push(0);
+            stack.Log();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack/ConsoleApp1/ConsoleApp1/stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Stack
+    {
+
+        int[] _quine;
+
+        public Stack(int[] buffer) 
+        {
+            _quine = buffer;
+        }
+
+        private void Update()
+        {
+            Array.Resize(ref _quine, _quine.Length + 1);
+        }
+
+        public void Push(int number)
+        {
+            Update();
+            _quine[^1] = number;
+        }
+        
+        public void Log()
+        {
+            for(int i = 0; i < _quine.Length; i++)
+            {
+                Console.WriteLine(_quine[i]);
+            }
+        }
+        
+    }
+}
