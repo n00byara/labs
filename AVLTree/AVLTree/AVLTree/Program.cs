3a09349c3c54d54331d557aaaa017c00aaa60ad3
n00byara <aminovmikl.01@gmail.com>
2021-08-18 00:52:15 +0300

    AVLTree first commit

--- AVLTree/AVLTree/AVLTree/Program.cs
+++ AVLTree/AVLTree/AVLTree/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace AVLTree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Stack<int> stack = new Stack<int>();
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
